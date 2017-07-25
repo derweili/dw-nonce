8c8ac51a0b260b7134d9d9e9aee4004b36f7d86b
derweili <jw@derweili.de>
2017-07-25 10:04:28 +0200

    Validator added
    Validator Class Added
    Test adapted for Validator

--- src/Validator.php
+++ src/Validator.php
@@ -4,11 +4,71 @@
  * @version 1.0
  * @author Julian Weiland
  **/
+
+namespace Derweili\DwNonces;
+
 class Validator
 {
 
+  private $config;
+
+  private $algorithm;
+
+  private $lifetime;
+
+  private $salt;
+
   function __construct()
   {
-    # code...
+    $config = new Config();
+
+    $this->algorithm    = $config->get_algorithm();
+    $this->lifetime     = $config->get_lifetime();
+    $this->salt         = $config->get_salt();
   }
+
+  /**
+   * @param string $nonce
+   * @param string|int $action
+   *
+   * @return bool|int
+   */
+  function verify( $nonce, $action ){
+
+    $this->nonce  = $nonce;
+    $this->action = $action;
+
+    if (empty($nonce)) {
+        return false;
+    }
+
+    $nonce_check = $this->get_encrypted( $this->get_reference() );
+    if ( hash_equals( $nonce_check, $nonce ) ) {
+        return 1;
+    }
+
+  }
+
+
+  private function get_encrypted( $message ){
+    return substr( hash_hmac( $this->algorithm, $message, $this->salt ), -12, 10 );
+  }
+
+  private function get_reference( $time_adjust = 0 ){
+
+    $string = $this->action;
+    $string .= ( $this->get_timesstamp( $time_adjust ) );
+    return $string;
+
+  }
+
+  /**
+   * @param int $time_adjust
+   * @return float
+   */
+  private function get_timesstamp( $time_adjust = 0 ){
+    return ceil(time() / ($this->lifetime / 2)) + $time_adjust;
+  }
+
+
 }
--- tests/test.php
+++ tests/test.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 
 use Derweili\DwNonces\Config;
 use Derweili\DwNonces\Nonce;
+use Derweili\DwNonces\Validator;
 
 $salt = 'Md[6@@zALZW_KNiBaZZx.d3+`Wx>DM01dmY[M-+)6V%+92J2OzsA{%(d$k%Qgc<t';
 
@@ -13,3 +14,10 @@ Config::set_salt($salt);
 $my_nonce = new Nonce( 'contact_form ');
 
 echo $my_nonce->get();
+echo '<br>';
+
+$test_nonce = '9b6141d7cb3';
+
+$validator = new Validator();
+
+echo $validator->verify('9b61417cb3', 'contact_form ');
